553404af51fee8aef186745a905550624997e700
Hylke Witjens <github@oizys.nl>
2022-07-28 10:25:11 +0200

    Removed debug code

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,3 @@ import {listToolbarActionRegistry} from 'sulu-admin-bundle/views';
 import TriggerToolbarAction from './views/List/toolbarActions/TriggerToolbarAction';
 
 listToolbarActionRegistry.add('verzameldwerk_akismet.trigger', TriggerToolbarAction);
-
-console.log(listToolbarActionRegistry.toolbarActions);
